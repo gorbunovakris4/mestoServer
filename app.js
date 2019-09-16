f11724ed3df9e623ace5047b7d4eee8dcdd675eb
Kristina Gorbunova <gorbunova.chr1stina@yandex.ru>
2019-09-16 09:59:17 +0300

    added frontend

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(express.static(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- public/index.js
+++
@@ -1,6 +0,0 @@
-import "./pages/index.css";
-import "./Api.js";
-import "./Card.js";
-import "./Cardlist.js";
-import "./Popup.js";
-import "./script.js";
