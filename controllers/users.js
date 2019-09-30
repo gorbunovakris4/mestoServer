8b19caed56dc21e8995257ac738cf99278433d4e
Kristina Gorbunova <gorbunova.chr1stina@yandex.ru>
2019-09-30 19:10:32 +0300

    done extra task

--- controllers/users.js
+++ controllers/users.js
@@ -12,10 +12,39 @@ module.exports.getUser = (req, res) => {
     .catch(() => res.status(404).send({ message: 'нет пользователя с таким id' }));
 };
 
-
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { newName, newAbout } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name: newName, about: newAbout },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.send({ message: 'Данные не прошли валидацию. Либо произошло что-то совсем немыслимое' }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { newAvatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: newAvatar },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.send({ message: 'Данные не прошли валидацию. Либо произошло что-то совсем немыслимое' }));
 };
